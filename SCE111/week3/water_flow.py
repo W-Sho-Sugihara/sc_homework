cbd80384f4c669dc0491f9780ee777719a27c0d9
W-Sho-Sugihara <shosmail31@gmail.com>
2026-03-21 19:06:38 +0900

    finished assignment

--- SCE111/week3/water_flow.py
+++ SCE111/week3/water_flow.py
@@ -1,12 +1,13 @@
+#added constants for water density, earths acceleration of gravity, ans water dynamic viscosity
+
 PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
 PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
 SUPPLY_VELOCITY = 1.65               # (meters / second)
 HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
 HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
 HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
-WATER_DENSITY=998.2                  # density of water (998.2 kilogram / meter^3)
-EARTH_ACCELERATION_OF_GRAVITY = 9.80665
 WATER_DENSITY = 998.2
+EARTH_ACCELERATION_OF_GRAVITY = 9.80665
 WATER_DYNAMIC_VISCOSITY = 0.0010016
 
 def main():
@@ -37,7 +38,7 @@ def main():
     print(f"Pressure at house: {pressure:.1f} kilopascals")
 
 def water_column_height(tower_height, tank_height):
-    return tower_height + 3 * tank_height / 4
+    return tower_height + (3 * tank_height) / 4
 
 def pressure_gain_from_water_height(height):
     return (WATER_DENSITY * EARTH_ACCELERATION_OF_GRAVITY * height) / 1000
@@ -48,15 +49,14 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
-    return -.04 * WATER_DENSITY * fluid_velocity * 2 * quantity_fittings / 2000
+    return -.04 * WATER_DENSITY * fluid_velocity ** 2 * quantity_fittings / 2000
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
-      #TODO: Need to implement
-      return 0
+      return (WATER_DENSITY * hydraulic_diameter * fluid_velocity) / WATER_DYNAMIC_VISCOSITY
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
-    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 + 1)
-    return -k * WATER_DENSITY * fluid_velocity ** 2 / 2000
+    k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 - 1)
+    return (-k * WATER_DENSITY * fluid_velocity ** 2) / 2000
 
 if __name__ == "__main__":
     main()
